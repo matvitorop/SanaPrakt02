08f20db1f4b520b43885f8ecd2c5a98b03fa5631
MatviiTorop <ipz224_tmd@student.ztu.edu.ua>
2022-11-12 14:10:09 +0200

    Змінено 2 останніх рядки коду

--- Task1/Task1.cpp
+++ Task1/Task1.cpp
@@ -9,7 +9,7 @@ int main()
     printf("for new branch text 222\n");
     printf("to check the result 333\n");
     printf("Курси Sana Commerce Software Development Bootcamp\n");
-    printf("Курси Sana Commerce Software Development Bootcamp\n");
-    printf("Курси Sana Commerce Software Development Bootcamp\n");
+    printf("the result will show 444\n");
+    printf("type of work in branches 555\n");
     return 0;
 }
