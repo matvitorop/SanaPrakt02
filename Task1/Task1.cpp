c35e86117e6b7a02a6cdc5949c2f07ba5d5f308b
MatviiTorop <ipz224_tmd@student.ztu.edu.ua>
2022-11-12 13:59:19 +0200

    Додано 5 дублікатів оператора

--- Task1/Task1.cpp
+++ Task1/Task1.cpp
@@ -6,5 +6,10 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     printf("Курси Sana Commerce Software Development Bootcamp\n");
+    printf("Курси Sana Commerce Software Development Bootcamp\n");
+    printf("Курси Sana Commerce Software Development Bootcamp\n");
+    printf("Курси Sana Commerce Software Development Bootcamp\n");
+    printf("Курси Sana Commerce Software Development Bootcamp\n");
+    printf("Курси Sana Commerce Software Development Bootcamp\n");
     return 0;
 }
